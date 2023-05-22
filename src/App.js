c178e60a7f7e1b99a8aaddb17d45f6eb1766d8f6
Happy Kumar <hkh7262@gmail.com>
2023-05-22 14:24:09 +0530

    scroll restoration effect added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  useLocation
 } from "react-router-dom";
 import NavBar from './components/NavBar';
 import Alert from './components/Alert';
@@ -13,6 +14,17 @@ import Features from './components/Features';
 import Footer from './components/Footer';
 import Terms from './components/Terms';
 import Privacy from './components/Privacy';
+
+// To handle the scroll restoration. It uses the useLocation hook from react-router-dom to listen to changes in the pathname of the URL. Whenever the pathname changes, the useEffect inside ScrollToTop is triggered
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const title = "Text Alter";
@@ -49,18 +61,20 @@ function App() {
   return (
     <>
     <Router>
-    <NavBar title={title} mode={mode} toggleMode={toggleMode}/>
-    <Alert alert={alert}/>
-    <div className="container my-5">
-      <Routes>
-        <Route exact path="/" element={<TextForm heading={title} mode={mode} alert={showAlert}/>}></Route>
-        <Route exact path="/about" element={<About title={title} mode={mode}/>}></Route>
-        <Route exact path="/features" element={<Features title={title} mode={mode}/>}></Route>
-        <Route exact path="/terms" element={<Terms title={title} mode={mode}/>}></Route>
-        <Route exact path="/privacy" element={<Privacy title={title} mode={mode}/>}></Route>
-      </Routes>
-    </div>
-    <Footer mode={mode}/>
+      {/* By rendering the ScrollToTop component inside the Router, it ensures that the scroll restoration effect is applied for all route changes. */}
+      <ScrollToTop />
+      <NavBar title={title} mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-5">
+        <Routes>
+          <Route exact path="/" element={<TextForm heading={title} mode={mode} alert={showAlert}/>}></Route>
+          <Route exact path="/about" element={<About title={title} mode={mode}/>}></Route>
+          <Route exact path="/features" element={<Features title={title} mode={mode}/>}></Route>
+          <Route exact path="/terms" element={<Terms title={title} mode={mode}/>}></Route>
+          <Route exact path="/privacy" element={<Privacy title={title} mode={mode}/>}></Route>
+        </Routes>
+      </div>
+      <Footer mode={mode}/>
     </Router>
     </>
   );
