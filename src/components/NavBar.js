1afb1381b39489e6f18cc2bc1441237687471403
Happy Kumar <hkh7262@gmail.com>
2023-06-05 16:13:57 +0530

    hamburger menu bug fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 export default function Navbar(props) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // track menu state
   const location = useLocation(); // get current location
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  const handleLinkClick = () => {
+    if (isMenuOpen) {
+      closeMenu();
+    }
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <div className="container-fluid">
-            <Link className="navbar-brand mx-1" to="/"><img src={logo} width={42} alt="logo"/></Link>
-            <Link className="navbar-brand ms-1 me-4" id="brand-name" to="/">{props.title}</Link>
+      <div className="container-fluid">
+        <Link className="navbar-brand mx-1" to="/">
+          <img src={logo} width={42} alt="logo" />
+        </Link>
+        <Link className="navbar-brand ms-1 me-4" id="brand-name" to="/">
+          {props.title}
+        </Link>
 
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={toggleMenu}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} aria-current="page" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`} to="/about">About</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className={`nav-link ${location.pathname === '/features' ? 'active' : ''}`} to="/features">Features</Link>
-                </li>
-                </ul>
+        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                aria-current="page"
+                to="/"
+                onClick={handleLinkClick}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`}
+                to="/about"
+                onClick={handleLinkClick}
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${location.pathname === '/features' ? 'active' : ''}`}
+                to="/features"
+                onClick={handleLinkClick}
+              >
+                Features
+              </Link>
+            </li>
+          </ul>
 
-                <div className={`nav-item form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                    <input className="form-check-input" type="checkbox" role="switch" id="darkMode" onClick={props.toggleMode}/>
-                    <label className="form-check-label" htmlFor="darkMode">Dark Mode</label>
-                </div>
-            </div>
+          <div className={`nav-item form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+            <input className="form-check-input" type="checkbox" role="switch" id="darkMode" onClick={props.toggleMode} />
+            <label className="form-check-label" htmlFor="darkMode">
+              Dark Mode
+            </label>
+          </div>
         </div>
+      </div>
     </nav>
-  )
+  );
 }
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 
 Navbar.defaultProps = {
-    title: 'Set Title',
-}
+  title: 'Set Title',
+};
